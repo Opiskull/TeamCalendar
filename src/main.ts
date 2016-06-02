354bfaa05c9ac26e278e85c5a6106486917469d0
Opiskull <Christian.Onuk@gmx.net>
2016-06-02 23:30:25 +0200

    refactor(all): move styles to picnic

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import 'bootstrap';
+import "../node_modules/picnic/releases/picnic.css!"
+import "../node_modules/picnic/releases/plugins.css!"
 import {Aurelia} from 'aurelia-framework';
 
 export function configure(aurelia: Aurelia) {
