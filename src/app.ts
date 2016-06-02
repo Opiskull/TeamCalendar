c4786a48e9d0fc19401a26311184f3bd6911ba3d
Opiskull <Christian.Onuk@gmx.net>
2016-06-02 23:27:04 +0200

    refactor(calendar-view): rename calendarview to calendarItem

--- src/app.ts
+++ src/app.ts
@@ -11,13 +11,13 @@ export class App {
   configureRouter(config: RouterConfiguration, router: Router) {
     config.title = 'TCal';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome', moduleId: 'welcome', nav: true, title: 'Welcome' },
+      { route: ['', 'welcome'], name: 'welcome', moduleId: 'welcome', title: 'Welcome' },
       { route: ['schedule/month', 'schedule/month/:month'], name: 'scheduleMonth', moduleId: 'calendars/scheduleMonth', nav: true, title: 'Schedule Month', href: "#/schedule/month" },
       { route: ['schedule/week', 'schedule/week/:week'], name: 'scheduleWeek', moduleId: 'calendars/scheduleWeek', nav: true, title: 'Schedule Week', href: "#/schedule/week" },
       { route: 'calendars', name: 'calendarsList', moduleId: 'calendars/list', nav: true, title: 'Calendars List' },
       { route: 'events/new', name: 'eventsNew', moduleId: 'calendars/events/newEvent', nav: true, title: 'New Event' },
-      { route: 'calendars/:id', name: 'calendarView', moduleId: 'calendarView', title: 'Calendar' }
-    ]);
+      { route: 'calendars/:id', name: 'calendarItem', moduleId: 'calendarItem', title: 'Calendar' }
+    ]);    
 
     this.router = router;
   }
--- src/calendarItem.ts
+++ src/calendarItem.ts
@@ -1,19 +1,24 @@
 import {Calendar} from "./calendars/models/calendar"
+import {Event} from "./calendars/models/event"
 import {CalendarService} from "./calendars/services/calendars"
-import {inject} from 'aurelia-framework'
+import {EventsService} from "./calendars/services/events"
+import {autoinject} from 'aurelia-framework'
 
-@inject(CalendarService)
+@autoinject
 export class CalendarView {
     private calendar: Calendar
     private currentDate: Date;
     private currentMonth: Date;
     private nextMonth: Date;
     private previousMonth: Date;
+    private events: Event[];
 
-    constructor(private calendarService: CalendarService) {
+    constructor(private calendarService: CalendarService, private eventsService: EventsService) {
+        
     }
 
     activate(routeParams) {
         this.calendar = this.calendarService.getById(routeParams.id);
+        this.events = this.eventsService.findEventsForCalendar(routeParams.id);
     }
 }
